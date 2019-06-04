0cc25c53c48d75d19d7858dedf5d3887a5b4167b
sagar.kadu <sagar.kadu@PVCO347.pervacio.com>
2019-06-04 16:40:54 +0530

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/testcases/TC_001.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+	public void testcase1()
+	{
+		System.setProperty("www.chrome.driver", "./Driver/chromedriver.exe" );
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.thetestingworld.com/");
+		driver.findElement(By.xpath("//*[@id=\"ja-search\"]/ul/li[1]/a")).click();
+		driver.findElement(By.id("username")).sendKeys("Sagar");
+		driver.findElement(By.id("password")).sendKeys("password");
+		driver.findElement(By.xpath("//*[@id=\"ja-content-main\"]/div/div/form/fieldset/button")).click();
+		driver.quit();
+	}
+}
